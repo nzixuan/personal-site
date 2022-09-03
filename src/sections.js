99602646562d46b65888d3e6261feb254c366d60
nzixuan <nzixuan@gmail.com>
2022-09-03 17:12:36 +0800

    Added link functionality

---
+++ src/sections.js
@@ -0,0 +1,17 @@
+const 
+
+export const sections = [
+{
+    id: "home", 
+    text: "Home",
+    url: "#home",
+    color: "white"
+},
+{
+    id: "projects", 
+    text: "Projects",
+    url: "#projects",
+    color: "white"
+}
+
+]
